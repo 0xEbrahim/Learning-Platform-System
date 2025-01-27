e8f90ff353d4f9185a37dacfd49e204dac7bb32f
Ebrahim <elsayedebrahim603@gmail.com>
2025-01-27 14:29:26 +0200

    chore: remove unnecessary blank line in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,6 @@ import app from "./app";
 import { connectDB } from "./config/db";
 import config from "./config/env";
 
-
 const server = app.listen(config.PORT, () => {
   console.log("Server started on port " + config.PORT);
   connectDB();
