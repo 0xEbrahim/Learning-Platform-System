10bb32e8c5dc8fa017993472a88bea00666fb2fc
Ebrahim <elsayedebrahim603@gmail.com>
2025-01-30 15:39:05 +0200

    feat: add fetching user profile | getting user by id

---
+++ backend/src/modules/users/user.controller.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import { asyncHandler } from "../../utils/asyncHandler";
+import { IUserRequset } from "../../interfaces/userRequest";
+import { userService } from "./user.service";
+import { IUser } from "./user.interface";
+import ApiError from "../../utils/ApiError";
+
+export const getUserInfo = asyncHandler(
+  async (req: IUserRequset, res: Response, next: NextFunction) => {
+    const userId: string = req.user?._id as string;
+    const user = await userService.getUserById(userId);
+    if (!user)
+      return next(new ApiError("Error while getting user profile", 401));
+    res.status(200).json({
+      status: "Success",
+      data: {
+        user,
+      },
+    });
+  }
+);
+
+export const getUserById = asyncHandler(
+  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const { id } = req.params;
+    const user = await userService.getUserById(id);
+    if (!user)
+      return next(new ApiError("Can't find the user with id: " + id, 404));
+    res.status(200).json({
+      status: "Success",
+      data: {
+        user,
+      },
+    });
+  }
+);
--- backend/src/modules/users/user.service.ts
+++ backend/src/modules/users/user.service.ts
@@ -1,7 +1,12 @@
-
+import { Document } from "mongoose";
+import { User } from "./user.model";
+import { IUser } from "./user.interface";
 
 class UserService {
-    
+  async getUserById(id: string): Promise<(Document & IUser) | null> {
+    const user: (Document & IUser) | null = await User.findById(id);
+    return user;
+  }
 }
 
-export const userService = new UserService()
+export const userService = new UserService();
