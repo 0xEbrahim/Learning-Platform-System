9b5271e4ff27d4ee1260690ece380e76c4ec3301
Ebrahim <elsayedebrahim603@gmail.com>
2025-02-02 14:32:03 +0200

    feat: update notification model to include message and status fields with default value

--- backend/src/modules/notifications/notifications.model.ts
+++ backend/src/modules/notifications/notifications.model.ts
@@ -1,22 +1,28 @@
 import mongoose, { Schema, Model } from "mongoose";
 import { INotification } from "./notification.interface";
 
-const notificationSchema: Schema<INotification> = new Schema({
-  title: {
-    type: String,
-    required: true,
+const notificationSchema: Schema<INotification> = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "unread",
+    },
   },
-  message: {
-    type: String,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-    default: "unread",
-  },
-}. {
-    timestamps: true
-});
+  {
+    timestamps: true,
+  }
+);
 
-export const Notification : Model<INotification> = mongoose.model("Notification", notificationSchema)
+export const Notification: Model<INotification> = mongoose.model(
+  "Notification",
+  notificationSchema
+);
